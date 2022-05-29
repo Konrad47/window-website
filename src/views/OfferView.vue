caa4b819ba312441783da6270a9a44500b428f8b
Konrad47 <messo123messo234@gmail.com>
2022-05-29 23:36:19 +0200

    create cards

--- src/views/OfferView.vue
+++ src/views/OfferView.vue
@@ -1,10 +1,48 @@
 <template>
-  <div>Offer</div>
+  <div id="windows">
+    <b-card-group v-for="(window, i) in getWindows" :key="i">
+      <b-card
+        border-variant="secondary"
+        :header="getWindows[i].size"
+        :title="getWindows[i].name"
+        :img-src="getWindows[i].image1"
+        img-top
+      >
+        <b-card-text> </b-card-text>
+      </b-card>
+    </b-card-group>
+  </div>
 </template>
 
 <script>
-export default {};
+export default {
+  name: "OfferView",
+  computed: {
+    getWindows() {
+      return this.$store.state.windows;
+    },
+  },
+  created() {
+    console.log(this.getWindows);
+  },
+};
 </script>
 
 <style scoped>
+.card {
+  margin: 20px;
+  box-shadow: 0 0 0.5em gray;
+}
+#windows {
+  display: inline-grid;
+  grid-template-columns: auto auto;
+  width: auto;
+}
+@media (max-width: 1200px) {
+  #windows {
+    display: inline-grid;
+    grid-template-columns: auto;
+    width: auto;
+  }
+}
 </style>
